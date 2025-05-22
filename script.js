26ef882b1722acb9574f3b9e24dabf01971218da
Mariana Souza <167915268+criandomari@users.noreply.github.com>
2025-05-22 13:09:38 -0300

    .

--- script.js
+++
@@ -1,6 +0,0 @@
-function abrirModal(img) {
-  let modal = document.createElement('div');
-  modal.classList.add('modal');
-  modal.innerHTML = `<div class="modal-content"><img src="${img.src}"><span onclick="this.parentElement.parentElement.remove()">Fechar</span></div>`;
-  document.body.appendChild(modal);
-}
